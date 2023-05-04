ef51c8cf891346b792a97b98f729aec44e8114c7
kedjzxc <hailrake27@gmail.com>
2023-05-04 11:18:43 +0300

    feat: task 4 done, but deletion of file has some problems

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require('fs');
+const path = require('path');
 
+const folderPath = path.join(__dirname, 'files');
+const copyFolderPath = path.join(__dirname, 'files-copy');
+
+function copyFiles() {
+  fs.readdir(folderPath, (err, files) => {
+    if (err) throw err;
+    files.forEach(file => {
+      fs.copyFile(path.join(folderPath, file), path.join(copyFolderPath, file), err => {
+        if (err) throw err;
+      });
+    });
+    console.log(`В папке находятся файлы: ${files}`);
+  });
+}
+
+fs.mkdir(copyFolderPath, { recursive: true }, err => {
+  if (err) throw err;
+  console.log('Папка создана');
+  copyFiles();
+});
+
+fs.watch(folderPath, { recursive: true }, (_, filename) => {
+  console.log(`Файл ${filename} был изменен`);
+  copyFiles();
+});
